7efa247d66e84fbfc9b3614ab9910c2963c838f3
Alex Hu <alexhubnds@icloud.com>
2018-04-23 23:05:18 -0400

    mq shit

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,12 @@ after_fork do |server, worker|
   # rescue Interrupt => _
   #   $rabbit_server.stop
   $server = FollowerServer.new("amqp://ivljoztm:XgUaHWXJXoB9sA3KUQuIPSjsV9kacqAg@eagle.rmq.cloudamqp.com/ivljoztm")
+  
+  uri = URI.parse(ENV['REDIS_FOLLOW'])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  uri2 = URI.parse(ENV['REDIS_USER'])
+  $redisUserServiceCache = Redis.new(:host => uri2.host, :port => uri2.port, :password => uri2.password)
+ 
 
   puts ' [x] Awaiting RPC requests'
   $server.start('rpc_queue')
